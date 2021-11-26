7a3f842a4d033349b43a5c5a11550b48696feb30
Daniel <dkilcullen25@gmail.com>
2021-11-26 01:46:45 -0500

    completed bio section and header, began content sections

--- script.js
+++ script.js
@@ -1,42 +1,84 @@
 window.onscroll = function() {scrollFunction()};
 
-const avi = document.getElementById("avi");
-const navArea = document.getElementById("navArea");
-const bio = document.querySelector("div#bio");
-const summary = document.querySelector("div#summary");
-const navLinksDiv = document.querySelector("#navLinks");
-const navLinks = document.querySelector("#navLinks ul");
+const avi = document.querySelector("div#nav div#navLeft img#avi");
+const nav = document.querySelector("div#nav");
+const navLeft = document.querySelector("div#navLeft");
+const navRight = document.querySelector("div#navRight");
+const header = document.querySelector("div#navLeft h1");
+const secondaryHeaders = document.querySelector("div#secondaryHeaders");
 const socials = document.querySelector("div#socials");
-const secondaryHeaders = document.querySelector("#secondaryHeaders");
+const navLinks = document.querySelector("div#navLinks");
+const navList = document.querySelector("ul#navList");
+
+const skills = document.querySelector("div#skills");
+const projects = document.querySelector("div#projects");
+
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    navArea.style.height = "100px";
-    bio.style.display = "none";
-    secondaryHeaders.style.display = "none";
-    summary.style.display = "flex";
-    summary.style.width = "100%";
-    avi.style.height = "80px";
-    avi.style.margin = "auto 15px auto 15px";
-    avi.style.left = "15px";
-    navLinksDiv.style.margin = "auto auto auto 30px";
-    navLinks.style.display = "flex";
-    // navLinks.style.float = "left";
-    navLinks.style.alignItems = "center";
-    navLinks.style.paddingBottom = "0px";
-    socials.style.marginLeft = "auto";
-  } else {
-    navArea.style.height = "100%";
-    bio.style.display = "block";
-    secondaryHeaders.style.display = "block";
-    summary.style.display = "block";
-    summary.style.width = "30%";
-    avi.style.height = "300px";
-    navLinksDiv.style.margin = "auto";
-    navLinks.style.display = "block";
-    // navLinks.style.float = "left";
-    navLinks.style.margin = "auto";
-    navLinks.style.paddingBottom = "10px";
-    socials.style.margin = "auto 15px auto 15px";
-  }
+    if (document.body.scrollTop > 100|| document.documentElement.scrollTop > 100) {
+        nav.style.height = "10vh";
+        nav.style.position = "fixed";
+        avi.style.height = "8vh";
+        avi.style.marginLeft = "15px";
+        avi.style.marginRight = "15px";
+        avi.style.paddingTop = "0px";
+        avi.style.paddingBottom = "0px";
+        navLeft.style.display = "flex";
+        navLeft.style.width = "100vw";
+        navLeft.style.height = "10vh";
+        navLeft.style.margin = "auto";
+        navRight.style.display = "none";
+        header.style.marginLeft = "15px";
+        secondaryHeaders.style.display = "none";
+        navLinks.style.margin = "auto";
+        navList.style.display = "flex";
+        navList.style.margin = "auto";
+        socials.style.display = "flex";
+        socials.style.marginRight = "25px";
+
+    } else {
+        nav.style.height = "100vh";
+        avi.style.height = "300px";
+        avi.style.margin = "auto";
+        avi.style.paddingTop = "10px";
+        avi.style.paddingBottom = "10px";
+        navLeft.style.display = "block";
+        navLeft.style.width = "30vw";
+        navLeft.style.height = "auto";
+        navRight.style.display = "block";
+        header.style.margin = "auto";
+        secondaryHeaders.style.display = "block";
+        navList.style.display = "block";
+        socials.style.display = "block";
+        socials.style.margin = "auto";
+
+    }
+
+    if (document.body.scrollTop > 100 && document.body.scrollTop < 200) {
+        document.getElementById('skills').scrollIntoView({
+            behavior: 'smooth'
+          });
+    }
 }
+
+function doScrolling(elementY, duration) { 
+    var startingY = window.pageYOffset;
+    var diff = elementY - startingY;
+    var start;
+  
+    // Bootstrap our animation - it will get called right before next frame shall be rendered.
+    window.requestAnimationFrame(function step(timestamp) {
+      if (!start) start = timestamp;
+      // Elapsed milliseconds since start of scrolling.
+      var time = timestamp - start;
+      // Get percent of completion in range [0, 1].
+      var percent = Math.min(time / duration, 1);
+  
+      window.scrollTo(0, startingY + diff * percent);
+  
+      // Proceed with animation as long as we wanted it to.
+      if (time < duration) {
+        window.requestAnimationFrame(step);
+      }
+    })
+  }
